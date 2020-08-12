ed328cc6b3b3c93fd72f0ee41187da639820b8b1
Katiuska <katiuskaalicea@gmail.com>
2020-08-11 18:23:39 -0700

    first commit

---
+++ team-builder/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+    This is the App
+    </div>
+  );
+}
+
+export default App;
